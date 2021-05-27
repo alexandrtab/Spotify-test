079d623c5618e2b338a6c0a87edf6a035b44cb57
Alex <shura.tabolich@mail.ru>
2021-05-27 19:46:08 +0300

    added burger menu and fixed bugs with adaptive

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-// $(document).ready(function(){
-//     $('.header-burger').click(function(event){
-// $('header-burger,.ul').toggleClass('active')
-//     })
-// })
-// let burger = document.getElementsByClassName('header-burger')
-// let ul = document.getElementsByClassName('ul')
-// function(addClass){
-// }
-// if(burger.click){function
+let burger = document.querySelector(".header-burger");
+let nav = document.querySelector(".nav");
 
-// }
-document.querySelector('.header-burger').onClick = () => {
-    document.querySelector('.nav').classList.toggle('active');
-    }
+burger.addEventListener("click", () => {
+nav.classList.toggle('active')
+});
