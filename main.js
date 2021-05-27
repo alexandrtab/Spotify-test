5619d80f3c590d8db6b191058a3a959d49f0c6f7
Alex <shura.tabolich@mail.ru>
2021-05-27 18:56:47 +0300

    added js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-let burger = document.getElementsByClassName("burger")
-let ul = document.getElementById("nav")
-if ($('. ').hasClass('on')) {
+// $(document).ready(function(){
+//     $('.header-burger').click(function(event){
+// $('header-burger,.ul').toggleClass('active')
+//     })
+// })
+// let burger = document.getElementsByClassName('header-burger')
+// let ul = document.getElementsByClassName('ul')
+// function(addClass){
+// }
+// if(burger.click){function
 
-}
+// }
+document.querySelector('.header-burger').onClick = () => {
+    document.querySelector('.nav').classList.toggle('active');
+    }
