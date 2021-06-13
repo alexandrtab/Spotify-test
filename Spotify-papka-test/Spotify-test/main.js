140d11c9c801b3ea616cc4f5eb092694201813b8
Alexandr <74628686+alexandrtab@users.noreply.github.com>
2021-06-13 22:55:21 +0300

    Add files via upload

--- Spotify-papka-test/Spotify-test/main.js
+++ Spotify-papka-test/Spotify-test/main.js
@@ -4,21 +4,3 @@ let nav = document.querySelector(".nav");
 burger.addEventListener("click", () => {
     nav.classList.toggle('active')
 });
-
-// $(document).ready(function(){
-//     $('.header-burger').click(function(event){
-// $('header-burger,.ul').toggleClass('active')
-//     })
-// })
-// let burger = document.getElementsByClassName('header-burger')
-// let ul = document.getElementsByClassName('ul')
-// function(addClass){
-// }
-// if(burger.click){function
-
-// }
-// document.querySelector('.header-burger').onClick = () => {
-//     document.querySelector('.nav').classList.toggle('active');
-// }
-
-// console.log('Hello World');
